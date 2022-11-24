79716456d0160a79af050ac1e0200f8c78350020
melvinsibichen <melvinsibichen1020@gmail.com>
2022-11-25 00:12:14 +0530

    booksearch created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { BooksearchComponent } from './booksearch/booksearch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminloginComponent
+    AdminloginComponent,
+    BooksearchComponent
   ],
   imports: [
     BrowserModule,
